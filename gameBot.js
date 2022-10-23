288a382f37e3aa1919c9ac00fc239886f2dfa166
mlksathil <67217921+mlksathil@users.noreply.github.com>
2022-10-23 21:37:08 +0530

    Refactor functions to appropriate Play Difficulty

--- gameBot.js
+++ gameBot.js
@@ -2,28 +2,42 @@ const botPlay = (function () {
 
     const _checkOnDifficulty = function () { return false }
 
+    const _miniMaxBotPlay = function () {
+
+        return [choosenSquare, choosenGrid]
+    }
+
+    const _randomBotPlay = function (currentActiveGrid) {
+        let squareOnChoosen, choosenSquare
+
+        if (currentActiveGrid.length > 1) {
+
+            var choosenGrid = currentActiveGrid[Math.floor(Math.random() * currentActiveGrid.length)]
+
+            squareOnChoosen = choosenGrid.querySelectorAll('.grid-smallSquare:not(.x-play, .o-play)')
+
+            choosenSquare = squareOnChoosen[Math.floor(Math.random() * squareOnChoosen.length)]
+
+        } else {
+
+            squareOnChoosen = currentActiveGrid.querySelectorAll('.grid-smallSquare:not(.x-play, .o-play)')
+
+            choosenSquare = squareOnChoosen[Math.floor(Math.random() * squareOnChoosen.length)]
+
+        }
+
+        return [choosenSquare, choosenGrid]
+    }
+
     return {
         updateBotTurn: function (botClass, currentActiveGrid) {
 
+            let choosenGrid, choosenSquare
+
             if (_checkOnDifficulty())
-                var currentCell = _miniMaxBotPlay()
-
-            let squareOnChoosen, choosenSquare
-
-            if (currentActiveGrid.length > 1) {
-
-                var choosenGrid = currentActiveGrid[Math.floor(Math.random() * currentActiveGrid.length)]
-
-                squareOnChoosen = choosenGrid.querySelectorAll('.grid-smallSquare:not(.x-play, .o-play)')
-
-                choosenSquare = squareOnChoosen[Math.floor(Math.random() * squareOnChoosen.length)]
-
-            } else {
-
-                squareOnChoosen = currentActiveGrid.querySelectorAll('.grid-smallSquare:not(.x-play, .o-play)')
-
-                choosenSquare = squareOnChoosen[Math.floor(Math.random() * squareOnChoosen.length)]
-
+                [choosenSquare, choosenGrid] = _miniMaxBotPlay(currentActiveGrid)
+            else {
+                [choosenSquare, choosenGrid] = _randomBotPlay(currentActiveGrid)
             }
 
             choosenSquare.classList.add(botClass)
